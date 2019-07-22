d4b257d29212a9668a5f36e565345eca3d00e2ca
cc66100644 <261923246@qq.com>
2019-07-22 23:43:35 +0800

    123

--- miniprogram/component/swiper/swiper.js
+++ miniprogram/component/swiper/swiper.js
@@ -14,19 +14,26 @@ Component({
    * 组件的初始数据
    */
   data: {
-      indicatorDots: true,
+      indicatorDots: false,
       autoplay: true,
       interval: 5000,
       duration: 1000,
       circular: true,
       indicatorcolor: "rgba(255, 255, 255, .3)",
-      indicatoractivecolor: "#ffffff"
+      indicatoractivecolor: "#ffffff",
+      swiperCurrent: 0,
   },
+
 
   /**
    * 组件的方法列表
    */
   methods: {
-
+    swiperChange: function (e) {
+      console.log(e);
+      this.setData({
+        swiperCurrent: e.detail.current   //获取当前轮播图片的下标
+      })
+    }
   }
 })
